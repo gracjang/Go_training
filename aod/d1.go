c83c4d3f7892a25c9a0f0d1dbbd97ce422ff4b9d
Gracjan Grochowski <gracjan.grochowski@meshsystems.com>
2023-08-02 17:34:36 +0200

    aod d1

--- aod/d1.go
+++ aod/d1.go
@@ -1,7 +1,51 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"os"
+	"sort"
+	"strconv"
+	"strings"
+)
 
 func main(){
-	fmt.Println("Hello World")
+	input, err := os.ReadFile("input.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	partOne(input)
+}
+
+func partOne(input []byte) {
+	lines := strings.Split(string(input), "\n")
+	elfCalories := []int{}
+	caloriesCounter := 0	
+
+	for _, line := range lines {
+		if line == "" {
+			elfCalories = append(elfCalories, caloriesCounter)
+			caloriesCounter = 0
+			continue
+		}
+
+		calories, _ := strconv.Atoi(line)
+		caloriesCounter += calories
+	}
+
+	max := max(elfCalories)
+	log.Println("Day1P1:", max)
+}
+
+func partTwo(input []byte) {
+	
+}
+
+func max(numbers []int) int {
+	if len(numbers) == 0 {
+		return 0
+	}
+
+	sort.Ints(numbers)
+	return numbers[len(numbers)-1]
 }
